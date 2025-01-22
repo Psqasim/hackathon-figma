f851334f63f5539a5fac13b72182789111652e87
Psqasim <muhammadqasim0326@gmail.com>
2025-01-22 10:40:42 +0500

    update

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,8 +1,7 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { product } from './products'
 import categories from './categories'
-import user from './user'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [product,categories,user],
+  types: [product,categories],
 }
